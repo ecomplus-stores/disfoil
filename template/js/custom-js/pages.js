d4fe4293e4b49101aaedd5b3220402160a5fe950
Matheus Reis <matheusgnreis@gmail.com>
2022-09-03 04:10:40 -0300

    chore: layout warn

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,1 +1,10 @@
 // Add your custom JavaScript for storefront pages here.
+storefront.on('widget:@ecomplus/widget-tag-manager', () =>{
+    setTimeout(() => {
+        const loginUser = window.ecomPassport.checkLogin()
+        if (!loginUser) {
+            console.log('entrei')
+            document.getElementById('user-button').href = '/app/#/checkout'
+        }
+    }, 800);
+})
