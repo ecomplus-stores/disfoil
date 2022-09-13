d01f53984e23ccf84ccc0dee354a35d5dc09a44b
Matheus Reis <matheusgnreis@gmail.com>
2022-09-13 12:11:31 -0300

    chore: update checkout

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,7 @@ module.exports = () => ({
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
       './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
-      // './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/js/EcCheckout.js'),
+      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/js/EcCheckout.js'),
       './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
     }
   }
