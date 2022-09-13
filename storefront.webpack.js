7086af3bbc9b993152aea4314a59efbb52951c6e
Matheus Reis <matheusgnreis@gmail.com>
2022-09-13 11:10:06 -0300

    chore: insert update

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -4,7 +4,10 @@ module.exports = () => ({
   resolve: {
     alias: {
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js')
+      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js'),
+      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+      // './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/js/EcCheckout.js'),
+      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
     }
   }
 })
