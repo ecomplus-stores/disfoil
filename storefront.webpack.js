f6fec604eee86e501bb26131d5d41e8cda8ef300
Matheus Reis <matheusgnreis@gmail.com>
2022-09-15 11:34:37 -0300

    chore: remove shipping

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,11 +3,11 @@ console.log(path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html'
 module.exports = () => ({
   resolve: {
     alias: {
-      './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
-      './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js'),
-      './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
-      './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/js/EcCheckout.js'),
-      './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
+      // './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
+      // './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js'),
+      // './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
+      // './js/EcCheckout.js': path.resolve(__dirname, 'template/js/custom-js/js/EcCheckout.js'),
+      // './html/TheProduct.html': path.resolve(__dirname, 'template/js/custom-js/html/TheProduct.html')
     }
   }
 })
