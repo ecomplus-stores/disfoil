b8680715b2fa2d8b53aefaaba9dff739a19f09f0
matheusgnreis <matheus@e-com.club>
2022-09-16 12:58:59 -0300

    chore: update layout

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,10 +1,20 @@
 // Add your custom JavaScript for storefront pages here.
+import { partytownSnippet } from '@builder.io/partytown/integration';
+const snippetText = partytownSnippet();
 storefront.on('widget:@ecomplus/widget-tag-manager', () =>{
     setTimeout(() => {
         const loginUser = window.ecomPassport.checkLogin()
         if (!loginUser) {
             console.log('entrei')
             document.getElementById('user-button').href = '/app/#/checkout'
+            document.getElementById('user-button').classList.add('logoff').remove('login')
+
+        } else {
+            console.log('sai')
+            document.getElementById('user-button').classList.add('login')
+        }
+        if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
+            document.getElementById('product-payment').classList.add('show')
         }
     }, 800);
 })
