c44c7b69fe68c3963504c93f2936ee8274abe9de
matheusgnreis <matheus@e-com.club>
2022-10-10 15:57:40 -0300

    chore: update store

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -1,15 +1,25 @@
 // Add your custom JavaScript for storefront pages here.
 storefront.on('widget:@ecomplus/widget-tag-manager', () =>{
     setTimeout(() => {
-        const loginUser = window.ecomPassport.checkLogin()
-        if (!loginUser) {
-            console.log('entrei')
-            document.getElementById('user-button').href = '/app/#/checkout'
-            document.getElementById('user-button').classList.add('logoff').remove('login')
-
-        } else {
-            console.log('sai')
-            document.getElementById('user-button').classList.add('login')
+        if (window.storefront) {
+            if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
+                const productPayment = document.querySelector('.product__prices')
+                console.log(productPayment)
+                const modalCalc = document.getElementById('modal-calc')
+                productPayment.insertAdjacentElement('afterend', modalCalc)
+            }
+            const loginUser = window.ecomPassport.checkLogin()
+            const userButton = document.getElementById('user-button')
+            if (!loginUser) {
+                console.log('entrei')
+                console.log(userButton)
+                userButton.href = '/app/#/checkout'
+                userButton.classList.add('logoff').remove('login')
+    
+            } else {
+                console.log('sai')
+                userButton.classList.add('login')
+            }
         }
     }, 800);
 })
