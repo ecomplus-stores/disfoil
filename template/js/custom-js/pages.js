4307d1b87edc3474f4347f672f8580f9382a530e
matheusgnreis <matheus@e-com.club>
2023-02-28 17:32:39 -0300

    chore: update store

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -4,22 +4,83 @@ storefront.on('widget:@ecomplus/widget-tag-manager', () =>{
         if (window.storefront) {
             if (window.storefront && window.storefront.context && window.storefront.context.resource === 'products') {
                 const productPayment = document.querySelector('.product__prices')
-                console.log(productPayment)
                 const modalCalc = document.getElementById('modal-calc')
                 productPayment.insertAdjacentElement('afterend', modalCalc)
             }
             const loginUser = window.ecomPassport.checkLogin()
             const userButton = document.getElementById('user-button')
             if (!loginUser) {
-                console.log('entrei')
-                console.log(userButton)
                 userButton.href = '/app/#/checkout'
-                userButton.classList.add('logoff').remove('login')
+                userButton.classList.add('logoff')
+                userButton.classList.remove('login')
     
             } else {
-                console.log('sai')
                 userButton.classList.add('login')
             }
         }
     }, 800);
+})
+
+let larg, comp
+$('#largura').change(function () {
+    larg = $(this).val()
+    if (larg && comp) {
+        var totalDim = larg * comp
+        $('#countMeters').text(totalDim)
+    }
+})
+$('#comprimento').change(function () {
+    comp = $(this).val()
+    if (larg && comp) {
+        var totalDim = larg * comp
+        $('#countMeters').text(totalDim)
+    }
+})
+comp = $('#comprimento').val()
+larg = $('#largura').val()
+if (larg && comp) {
+    var totalDim = larg * comp
+    $('#countMeters').text(totalDim)
+}
+setTimeout(function () {
+    $('#checked').change(function () {
+        var input1 = $('#defaultCheck1')
+        var input2 = $('#defaultCheck2')
+        var totalAcrescomp = $('#comprimento').val()
+        larg = $('#largura').val()
+        if (larg && comp) {
+            var totalDim = larg * comp
+        }
+        if (input1[0].checked && input1[0].value) {
+            totalAcres = Math.ceil(1.15 * totalDim)
+            if (input2[0].checked && input2[0].value) {
+                totalAcres = Math.ceil(1.25 * totalDim)
+            }
+        }
+        if (input2[0].checked && input2[0].value) {
+            totalAcres = Math.ceil(1.10 * totalDim)
+            if (input1[0].checked && input1[0].value) {
+                totalAcres = Math.ceil(1.25 * totalDim)
+            }
+        } if (!input1[0].checked && !input2[0].checked) {
+            totalAcres = totalDim
+        } $('#countMeters').text(totalAcres)
+    })
+}, 500)
+$('#saveModal').click(function () {
+    const nomeProduto = storefront.context.body.name.split(' ')
+    var metragem = parseFloat(nomeProduto[nomeProduto.length - 1].replace('m2', '')) || 1
+    var meters = parseInt($('#countMeters').text())
+    console.log(metragem)
+    console.log(meters)
+    let quant = Math.ceil(meters / metragem)
+    console.log(quant)
+    console.log(typeof quant)
+    $('.quantity-selector__input').val(Number(quant))
+})
+$('#clearModal').click(function () {
+    document.getElementById('myForm0').reset()
+    document.getElementById('myForm1').reset()
+    document.getElementById('checked').reset()
+    $('#countMeters').text('0')
 })
