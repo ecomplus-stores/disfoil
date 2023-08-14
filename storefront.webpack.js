6c8bc923b40cfbb524b78baba3f1f33d52f80088
matheusgnreis <matheus@e-com.club>
2023-08-14 10:56:49 -0300

    chore: update product

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -8,6 +8,7 @@ module.exports = () => ({
       './methods/set-search-term': path.resolve(dirSearchAlias, 'set-search-term'),
       './html/ShippingCalculator.html': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.html'),
       './html/APrices.html': path.resolve(__dirname, 'template/js/custom-js/html/APrices.html'),
+      './js/APrices.js': path.resolve(__dirname, 'template/js/custom-js/js/APrices.js'),
       './js/ShippingCalculator.js': path.resolve(__dirname, 'template/js/custom-js/html/ShippingCalculator.js'),
       './js/TheProduct.js': path.resolve(__dirname, 'template/js/custom-js/js/TheProduct.js'),
       './js/CartItem.js': path.resolve(__dirname, 'template/js/custom-js/js/CartItem.js'),
